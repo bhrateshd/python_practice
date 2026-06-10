05d78881d75a175bd72233fc36f695ac1019b734
Bhratesh Dhangar <109329115+bhrateshd@users.noreply.github.com>
2026-06-10 09:30:23 +0000

    addinmg second code

---
+++ comments_ecapesec.py
@@ -0,0 +1,6 @@
+
+# This is a comment we use "#"" for comments
+# this is another comment
+# we can use ctr + forword slash / to comment multiple lines at once 
+
+print("Hey this is a test print statement ")
