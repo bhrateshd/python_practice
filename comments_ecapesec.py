4aa1efc4b12a7b94985f4e082f74b9d8e57eac68
Bhratesh Dhangar <109329115+bhrateshd@users.noreply.github.com>
2026-06-10 09:39:25 +0000

    multi line comment

--- comments_ecapesec.py
+++ comments_ecapesec.py
@@ -5,3 +5,10 @@
 
 print("Hey this is a test print statement ")
 print("this file is for comments and escape Sequences ")
+
+print("Below is a multi line comment")
+
+'''
+This is a multi line comment
+this is another line 
+'''
