dcde28920d977f45c33d1ca76c30c4832792e130
Bhratesh Dhangar <109329115+bhrateshd@users.noreply.github.com>
2026-06-11 08:29:46 +0000

    adding string slicing

--- 7_string.py
+++ 7_string.py
@@ -20,3 +20,22 @@ print(name[3])
 
 for character in apple:
     print(character)
+
+
+# String slicing and Operations on string
+
+names = "Bhratesh,Manthan"
+print(name[0:5])
+
+
+
+# We can find the length of any string using len function 
+
+fruit = "Mango"
+mangoLen = len(fruit)
+
+print(mangoLen)
+
+print(fruit[0:4])
+print(fruit[:4])
+print(fruit[:])
