0228bb5c581ca8fdccf3767b157ebdb7482db3a4
Bhratesh Dhangar <109329115+bhrateshd@users.noreply.github.com>
2026-06-11 06:01:56 +0000

    adding typecasting

---
+++ typecasting.py
@@ -0,0 +1,23 @@
+# A convsirsion of one datatype into another is called Typecatsing in python
+# the numbers we have written in  doublke "" is a string
+# there are tow types of typecasting implicit and explicit
+
+
+# Below is the example of explicit typecasting 
+# here we are asking python to convert datatype from string to a int
+a = "1"
+# a = 1
+
+b = "2"
+# b = 2
+
+print(int(a)+int(b))
+
+
+# Implicit Typecasting
+# One datatype is converted automatically into another is called implicit typecasting
+# here below python will convert both in float and give result in float
+c = 1.9
+d = 8
+
+print(c+d)
