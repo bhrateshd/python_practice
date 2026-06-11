041b9b8f95bbe5fddd428eda286c0ad3899e4e51
Bhratesh Dhangar <109329115+bhrateshd@users.noreply.github.com>
2026-06-11 07:20:00 +0000

    renaming files

---
+++ 7_string.py
@@ -0,0 +1,16 @@
+# Index starts from 0
+
+name = "Bhratesh"
+friend = "Manthan"
+anotherFriend = 'Rohit'
+
+apple = '''He Said, 
+Hi Bhratesh
+hey I am good
+I want to eat and apple'''
+
+print("Hello, " + name)
+print(apple)
+
+# Printg index 0 of name
+print(name[0])
