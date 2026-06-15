13089a31f57fe192883d0626a3f260e0a1cba725
Bhratesh Dhangar <109329115+bhrateshd@users.noreply.github.com>
2026-06-15 05:33:24 +0000

    Adding a if else statements

---
+++ 8_if_else_statements.py
@@ -0,0 +1,34 @@
+# a = int(input("Enter your age: "))
+# print("Your age is:", a)
+
+
+# Conditional operators 
+# >, << >=, <=, ==
+# print(a>18)
+# print(a<=18)
+# print(a>=18)
+# print(a==18)
+# print(a!=19)
+
+
+# if (a>18):
+#     print("You can drive")
+# else:
+#     print("You cannot drive")
+
+# ==================================================================================== #
+
+# elif 
+
+num = int(input("Enter the value oif num: "))
+
+if (num < 0):
+    print("Number is negative")
+elif (num == 0):
+    print("Number is zero")
+elif (num == 999):
+    print("Number is special")
+else:
+    print("Number is positive")
+
+print("I am Happy Now")
