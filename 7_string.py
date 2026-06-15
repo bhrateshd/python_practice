63ae685beec6faeebbc8c6ab7a6a19e277532908
Bhratesh Dhangar <109329115+bhrateshd@users.noreply.github.com>
2026-06-15 04:12:20 +0000

    adding string metghods

--- 7_string.py
+++ 7_string.py
@@ -1,41 +1,53 @@
-# Index starts from 0
+# # Index starts from 0
 
-name = "Bhratesh"
-friend = "Manthan"
-anotherFriend = 'Rohit'
+# name = "Bhratesh"
+# friend = "Manthan"
+# anotherFriend = 'Rohit'
 
-apple = '''He Said, 
-Hi Bhratesh
-hey I am good
-I want to eat and apple'''
+# apple = '''He Said, 
+# Hi Bhratesh
+# hey I am good
+# I want to eat and apple'''
 
-print("Hello, " + name)
-print(apple)
+# print("Hello, " + name)
+# print(apple)
 
-# Printg index 0 of name
-print(name[0])
-print(name[1])
-print(name[2])
-print(name[3])
+# # Printg index 0 of name
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
 
-for character in apple:
-    print(character)
-
-
-# String slicing and Operations on string
-
-names = "Bhratesh,Manthan"
-print(name[0:5])
+# for character in apple:
+#     print(character)
 
 
+# # String slicing and Operations on string
 
-# We can find the length of any string using len function 
+# names = "Bhratesh,Manthan"
+# print(name[0:5])
 
-fruit = "Mango"
-mangoLen = len(fruit)
 
-print(mangoLen)
 
-print(fruit[0:4])
-print(fruit[:4])
-print(fruit[:])
+# # We can find the length of any string using len function 
+
+# fruit = "Mango"
+# mangoLen = len(fruit)
+
+# print(mangoLen)
+
+# print(fruit[0:4])
+# print(fruit[:4])
+# print(fruit[:])
+
+
+# String Methods 
+# Strings are immutable - you cant change strings you can make another copy of that
+
+a = "Bhratesh!!!"
+print(len(a))
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("Bhratesh", "John"))
+print(a.split(" "))
