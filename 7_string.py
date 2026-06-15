b9b49b627d7ba1621ee6f6da646feef343b4bffc
Bhratesh Dhangar <109329115+bhrateshd@users.noreply.github.com>
2026-06-15 04:35:56 +0000

    added stringh methods

--- 7_string.py
+++ 7_string.py
@@ -44,10 +44,20 @@
 # String Methods 
 # Strings are immutable - you cant change strings you can make another copy of that
 
-a = "Bhratesh!!!"
-print(len(a))
-print(a.upper())
-print(a.lower())
-print(a.rstrip("!"))
-print(a.replace("Bhratesh", "John"))
-print(a.split(" "))
+# a = "Bhratesh!!!"
+# print(len(a))
+# print(a.upper())
+# print(a.lower())
+# print(a.rstrip("!"))
+# print(a.replace("Bhratesh", "John"))
+# print(a.split(" "))
+
+
+# find method is used to print the index
+str1 = "He's name is John. He is a honest man."
+print(str1.find("is")) # this will written -1 is it dosent find the exact sgtring 
+# print(str1.index("ishh")) # it will print error 
+
+# is lower = it will print true or false 
+str1 = "Welcome Bhratesh"
+print(str1.islower())
