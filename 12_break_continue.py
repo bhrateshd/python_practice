48a431fa166b2865f896db789e462547f6f4c866
Bhratesh Dhangar <109329115+bhrateshd@users.noreply.github.com>
2026-06-16 06:18:16 +0000

    adding continue statement

--- 12_break_continue.py
+++ 12_break_continue.py
@@ -1,9 +1,23 @@
-# break and Continue statements 
+# # break and Continue statements 
 
+# for i in range(12):
+#     if(i == 10):
+#         break
+#     print("5 X", i+1, "=", 5 * (i+1))
+        
+
+# print("Exiting the loop")
+
+
+
+# Continue statement
 for i in range(12):
     if(i == 10):
-        break
-    print("5 X", i+1, "=", 5 * (i+1))
+        print("Skip the iteration")
+        continue
+    print("5 X", i+1, "=", 5 * i)
         
 
 print("Exiting the loop")
+
+
